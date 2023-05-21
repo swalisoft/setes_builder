8e6c1d22a8b158fa8215045e50a41e803388e6bc
Mita <miguelmita100121144@gmail.com>
2023-05-21 09:59:08 -0400

    interfaces of create, edith and list

--- api/hello.py
+++ api/hello.py
@@ -1,7 +1,26 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/servers')
 def hello():
+    os.system('useradd nombe ')
+ 
+    # guardar en la base de datao
+    return 'Hello Work'
+
+@app.route('/listar')
+def hello():
+    # guardar en la base de datao
+    return 'Hello Work'
+
+@app.route('/usuarios')
+def hello():
+    # guardar en la base de datao
+    return 'Hello Work'
+
+@app.route('/login')
+def hello():
+    # guardar en la base de datao
     return 'Hello Work'
