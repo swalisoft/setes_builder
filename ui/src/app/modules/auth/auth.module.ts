d1b47f55f361a51c11a7da6779e6458a7c4ba97e
J Carlos <jhomanidev@gmail.com>
2023-05-21 05:47:12 -0300

    add requrements txt

--- ui/src/app/modules/auth/auth.module.ts
+++ ui/src/app/modules/auth/auth.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
-    HomeComponent,
   ],
   imports: [
     CommonModule,
   ],
-  exports: [
-  ]
 })
 export class AuthModule { }
