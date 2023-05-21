f12aa954b95433b80438f1375644a2f7c90b1be0
padilla <aronpad123@gmail.com>
2023-05-21 19:43:39 +0200

    configuracion database servidor

--- api/create_virtual_host.py
+++ api/create_virtual_host.py
@@ -1,5 +1,7 @@
 import os
 from Postgres import Postgres
+import psycopg2
+postgres = Postgres()
 
 ## add user to sudo group
 # sudo usermod -aG wheel Patilla
@@ -8,8 +10,12 @@ from Postgres import Postgres
 def create_virtualhost(host, nombre):
   with open('assets/vhost_template.conf', 'r') as f:
     data = f.read()
-    data.replace("$username$", nombre)
-    data.replace("$domain$", host)
+    data = data.replace("$username$", nombre)
+    data = data.replace("$domain$", host)
+  
+  with open(f'/etc/apache2/vhosts.d/{host}.conf', 'w') as f:
+    f.write(data)
+    
 
 #create_virtualhost("mita@gmail.com","mita")
 def create_server_user(passwds,users):
@@ -19,6 +25,9 @@ create_server_user("34353435","joel")
 
 
 def create_database(user,passwd,database):
-  postgres.execute(f'CREATE USER {user} WITH ENCRYPTED PASSWORD {passwd}')
-  postgres.execute(f'GRANT ALL PRIVILEGES ON DATABASE {database} TO {user}') 
-  
+  postgres.execute(f'create database {database}')
+  postgres.execute(f"CREATE USER {user} WITH ENCRYPTED PASSWORD '{passwd}'")
+  postgres.execute(f'GRANT ALL PRIVILEGES ON DATABASE {database} TO {user}')
+  postgres.execute(f'GRANT ALL ON schema public TO {user}')
+
+create_database("boris","34353435","biblioteca")
