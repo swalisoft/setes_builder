6d4799b876dcb0f3e099562f073572ca385157d3
J Carlos <jhomanidev@gmail.com>
2023-05-28 14:43:16 -0300

    add user Id

--- api/app.py
+++ api/app.py
@@ -36,18 +36,14 @@ def servers():
         data['domain'],
         data['db_user'],
         data['db_password'],
-        data['database']
+        data['database'],
+        data['user_id']
       )
     )
 
     response = jsonify({"messaage":  "succesful crated"})
 
     return response
-
-@app.route('/users')
-def users():
-  # guardar en la base de datao
-  return 'Hello Work'
 
 @app.route('/login', methods=['POST'])
 def login():
